386b6bb1d0dfb25dc3cfc09df416b780bf8a886f
juliobsilva <jooble111@gmail.com>
2019-04-11 20:57:57 -0300

    Ajustando o sistema

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
