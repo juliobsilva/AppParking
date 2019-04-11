a529f183ab27cf17e48a66895e61932bc56997c9
juliobsilva <jooble111@gmail.com>
2019-04-11 20:37:23 -0300

    Ajustando o sistema

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -133,9 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #    os.path.join(BASE_DIR, "static"),
 #]
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 
 LOGIN_URL = 'login'
