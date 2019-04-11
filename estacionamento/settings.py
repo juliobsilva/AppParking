b1001ff9b2d01426d213c000d8452167abb69878
juliobsilva <jooble111@gmail.com>
2019-04-11 20:32:27 -0300

    Ajustando o sistema

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -129,9 +129,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'core_home'
